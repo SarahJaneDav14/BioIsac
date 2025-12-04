7fb81d3d472d97b96e6b1f4838740866e0d9f741
Gavinsudomir <gmsudomir@crimson.ua.edu>
2025-12-03 21:34:17 -0600

    fixed log in issues and added git ignore

--- api/Program.cs
+++ api/Program.cs
@@ -24,8 +24,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+else
+{
 app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 app.UseCors("OpenPolicy");
